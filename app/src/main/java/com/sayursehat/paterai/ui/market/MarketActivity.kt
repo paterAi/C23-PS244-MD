66962808266135fcd7ddf63a4be09f0ca2dea6f2
Fariz Hustha <farizhustha205@gmail.com>
2023-05-31 18:24:16 +0700

    Update Market Activity With Custom Bottom App Bar and Assets

--- app/src/main/java/com/sayursehat/paterai/ui/market/MarketActivity.kt
+++ app/src/main/java/com/sayursehat/paterai/ui/market/MarketActivity.kt
@@ -1,12 +1,40 @@
 package com.sayursehat.paterai.ui.market
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sayursehat.paterai.R
+import com.sayursehat.paterai.databinding.ActivityMarketBinding
 
 class MarketActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMarketBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_market)
+
+        binding = ActivityMarketBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navView: BottomNavigationView = binding.bottomNavMarket
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_market)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_product,
+                R.id.navigation_order,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
